a2a98b3ff722dfb7c5ccae97d9d783a804275d85
Pham Đức Hiếu <hieuhd610@gmail.com>
2026-02-16 22:34:33 +0900

    Add files via upload

---
+++ btvn01.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class btvn01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // nhập số lượng phần tử
+        System.out.println("Nhập số phần tử: ");
+        int n = sc.nextInt();
+
+        int [] arr = new int[n];
+
+        // nhập mảng
+        System.out.println("Nhập các phần tử ");
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        // selection giảm dần
+        for (int i = 0; i < n - 1; i++){
+            int maxIndex = i; // giả sử phần tử đầu tiên là lớn nhất
+            for (int j = i+1; j < n; j++){
+                if (arr[j] > arr[maxIndex]) {
+                    maxIndex = j ; // cập nhật vị trí có phần tử lớn nhất
+                }
+            }
+
+            // Hoán đổi
+            int temp = arr[maxIndex];
+            arr[maxIndex] = arr[i];
+            arr[i] = temp;
+
+        }
+
+        // sau khi sắp xếp, phần tử đầu tiên là lớn nhất
+        System.out.println("Phần tử lớn nhất là: " + arr[0]);
+        sc.close();
+    }
+}
+
